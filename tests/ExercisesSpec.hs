284188f41bc808ead831437da5c8f85fe6bcfd7f
Jongsoo Lee <jongsoo.lee@lge.com>
2015-08-25 09:52:01 +0900

    fix some warning

--- tests/ExercisesSpec.hs
+++ tests/ExercisesSpec.hs
@@ -1,7 +1,6 @@
 module ExercisesSpec (spec) where
 
 import Test.Hspec
-import Test.Hspec.QuickCheck
 
 import Exercises
 
@@ -9,16 +8,14 @@ spec :: Spec
 spec = do
   describe "myLast" $ do
     it "myLast" $ do
-      myLast [1..10] == 10
-      myLast [1] == 1
+      myLast [1..10] `shouldBe` (10 :: Int)
   describe "myButLast" $ do
     it "myButLast" $ do
-      myButLast [1..10] == 9
-      myButLast [1..3] == 2
+      myButLast [1..10] `shouldBe` (9 :: Int)
   describe "insertAt" $ do
     it "insertAt" $ do
       insertAt 'X' "abcde" 2 == "aXbcde"
   describe "range" $ do
     it "range" $ do
-      range 1 10 == [1..10]
+      range 1 10 `shouldBe` ([1..10] :: [Int])
 

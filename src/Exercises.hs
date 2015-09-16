60b62ed634f38c3369de15ef49d70b9b964530cb
Jooyung Han <jooyung.han@gmail.com>
2015-09-16 16:05:06 +0900

    Fix lint warnings

--- src/Exercises.hs
+++ src/Exercises.hs
@@ -2,33 +2,33 @@ module Exercises where
 
 import Control.Monad
 import Data.Array.IO
-import Data.Functor
 import System.Random (randomRIO)
 import Data.List (group, subsequences)
 import Control.Arrow ((&&&))
-
--- 이 파일은 모임 때 함께 풀어나갈 파일입니다.
 
 -- Problem 1 ~ 10 : Lists --
 
 -- 1
 myLast :: [a] -> a
+myLast [] = undefined
 myLast [x] = x
-myLast (x:xs) = myLast xs
+myLast (_:xs) = myLast xs
 
 -- 2
 myButLast :: [a] -> a
-myButLast [x1,x2] = x1
-myButLast (x:xs) = myButLast xs
+myButLast [] = undefined
+myButLast [x1,_] = x1
+myButLast (_:xs) = myButLast xs
 
 -- 3
 elementAt :: [a] -> Int -> a
-elementAt (a:as) 0 = a
-elementAt (a:as) n = elementAt as (n-1)
+elementAt (a:_) 0 = a
+elementAt [] _ = undefined
+elementAt (_:as) n = elementAt as (n-1)
 
 -- 4
 myLength :: [a] -> Int
-myLength = foldr (\a x -> x + 1) 0
+myLength = foldr (\_ x -> x + 1) 0
 
 -- 5
 myReverse :: [a] -> [a]
@@ -78,11 +78,11 @@ decodeModified = concatMap f
 encodeDirect :: (Eq a) => [a] -> [Enc a]
 encodeDirect [] = []
 encodeDirect (x:xs) = toInc n : encodeDirect remainder
-                          where (n,remainder) = count xs 1
+                          where (n, remainder) = count xs 1
                                 toInc 1 = Single x
-                                toInc n = Multiple n x
-                                count [] n = (n, [])
-                                count all@(a:as) n = if a == x then count as (n+1) else (n, all)
+                                toInc n' = Multiple n' x
+                                count [] n' = (n', [])
+                                count t@(a:as) n' = if a == x then count as (n'+1) else (n, t)
 
 -- 14
 dupli :: [a] -> [a]
@@ -93,21 +93,23 @@ repli :: [a] -> Int -> [a]
 repli xs n = concatMap (replicate n) xs
 
 -- 16
-partition n [] = []
+partition :: Int -> [a] -> [[a]]
+partition _ [] = []
 partition n xs = let (h,t) = splitAt n xs
                  in h : partition n t
 
 dropEvery :: [a] -> Int -> [a]
-dropEvery [] n = []
+dropEvery [] _ = []
 dropEvery xs n = concatMap (take (n - 1)) $ partition n xs
 
 -- 17
 split :: [a] -> Int -> ([a],[a])
-split [] n = ([], [])
+split [] _ = ([], [])
 split xs n = (take n xs, drop n xs)
 
 -- 18
-slice [] n m = []
+slice :: [a] -> Int -> Int -> [a]
+slice [] _ _ = []
 slice xs n m = drop (n-1) $ take m xs
 
 -- 19
@@ -120,6 +122,7 @@ removeAt :: [a] -> Int -> (a, [a])
 removeAt (x:xs) 1 = (x, xs)
 removeAt (x:xs) n = let (a,as) = removeAt xs (n-1)
                     in (a, x:as)
+removeAt _ _ = undefined
 
 -- Problem 21 ~ 28 : Lists again --
 -- 21
@@ -139,23 +142,24 @@ rndSelect as n = take n <$> shuffle as
 
 shuffle :: [a] -> IO [a]
 shuffle xs = do
-        ar <- newArray n xs
-        forM [1..n] $ \i -> do
-            j <- randomRIO (i,n)
-            vi <- readArray ar i
-            vj <- readArray ar j
-            writeArray ar j vi
-            return vj
-  where
-    n = length xs
-    newArray :: Int -> [a] -> IO (IOArray Int a)
-    newArray n =  newListArray (1, n)
+                ar <- newArray' n xs
+                forM [1..n] $ \i -> do
+                    j <- randomRIO (i,n)
+                    vi <- readArray ar i
+                    vj <- readArray ar j
+                    writeArray ar j vi
+                    return vj
+            where
+              n = length xs
+              newArray' :: Int -> [a] -> IO (IOArray Int a)
+              newArray' n' =  newListArray (1, n')
 
 -- 24
 diffSelect :: Int -> Int -> IO [Int]
 diffSelect n m = rndSelect [1..m] n
 
 -- 25
+rndPermu :: [a] -> IO [a]
 rndPermu = shuffle
 
 -- 26
